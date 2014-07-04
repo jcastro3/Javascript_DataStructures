d1859d0573ae2c75572c36c9a29788492d45db91
Jesus Castro <jcastro@nearsoft.com>
2014-07-04 16:15:32 -0700

    refactorized UI components and added index.js

--- UI.js
+++ UI.js
@@ -1,139 +1,117 @@
-var objArray = [],
-    stack,
-    queue;
-
-$(document).ready(function () {
+var collections = (function ($, c) {
     'use strict';
-    var UI = (function () {
-        
+    function UI() {
+
         function clearTxt() {
             $('.clearTxt').val('');
         }
         //PRIVATE MEMBERS
-        function createNewObj(name, typeSelected, obj, item) {
-            var key;
-            
+        function createNewObj(name, typeSelected, item) {
+            var key, tmp, stack;
+
             switch (typeSelected) {
             case 'stack':
-          
-                $('#obj_ul').data(name, { stack_val : obj = new collections.Stack()});
-                stack = $('#obj_ul').data(name).stack_val;
-                
+                item.data('collection', { type: 'stack', name: name, value: new collections.Stack() });
+                $("#stack_ul").text('');
                 break;
+
             case 'queue':
-                    
-                $('#obj_ul').data(name, { queue_val : obj = new collections.Queue()});
-                queue = $('#obj_ul').data(name).queue_val;
+                item.data('collection', { type: 'queue', name: name, value: new collections.Queue() });
+                $("#queue_ul").text('');
                 break;
-                    
+
             default:
                 alert('Unable to process you petition');
-                
+
             }
+
+            $('.' + typeSelected + '-active').removeClass(typeSelected + '-active');
+            item.addClass(typeSelected + '-active');
             $('#' + typeSelected + '_name').text(name);
+
             $('#obj_ul').prepend(item);
             clearTxt();
 
         }
-        
-        
-        
-        function updateList(obj, type, name) {
-            
-            var tmp = obj.top;
-            $('#' + type + '_ul').text('');
-            while (tmp) {
-                $('#' + type + '_ul').append(tmp.data);
-                tmp = tmp.next;
-                $('#' + type + '_name').text(name);
-            }
-        }
-        
-        function updateQueue(queue) {
-            var tmp = queue.front;
-            $('#queue_ul').text('');
-            while (tmp) {
-                $('#queue_ul').append(tmp.data);
-                tmp = tmp.next;
 
+
+        function updateList(c) {
+
+            var tmp = c.value.top;
+            $('#' + c.type + '_ul').text('');
+            $('#' + c.type + '_name').text(c.name);
+            while (tmp) {
+                $('#' + c.type + '_ul').append(tmp.data);
+                tmp = tmp.next;
             }
         }
 
-        //PUBLIC MEMBERS
         $(document).on('click', '.obj_li', function () {
-            var i,
-                name = $(this).attr('data-collection-name'),
-                length = objArray.length,
-                type = $(this).attr('type');
-            
-            if (type === 'stack') {
-                stack = $('#obj_ul').data(name).stack_val;
-                updateList(stack, type, name);
-            } else {
-                queue = $('#obj_ul').data(name).queue_val;
-                updateList(queue, type, name);
-            }
-                                         
-            
-            
-                
-            
-            
+            var c = $(this).data('collection');
+
+            $('.' + c.type + '-active').removeClass(c.type + '-active');
+            $(this).addClass(c.type + '-active');
+            updateList(c);
+
         });
-        
+
+
         $('#create_btn').click(function () {
             var name,
                 item,
                 selected,
                 msg;
-            
+
             name = $('#create_newInstance').val();
             selected = $('#typeSelected').val();
-            
+
             if (name === '') {
                 return alert('empty field, please insert value');
             }
-            
-            item = $('<li class=obj_li>' + name + ' [' + selected + ']</li>')
-                                                                            .attr('data-collection-name', name)
-                                                                            .attr('type', selected);
-            
-            (selected === 'stack') ? createNewObj(name, selected, stack, item) : createNewObj(name, selected, queue, item);
-            
+
+            item = $('<li class=obj_li>' + name + ' [' + selected + ']</li>').data('flag', 'on');
+
+            createNewObj(name, selected, item);
+
         });
-        
-        
-        
+
+
+
         $('#push_btn').click(function () {
-            var push_val, item;
+            var push_val,
+                item,
+                c = $('.stack-active').data('collection').value;
+
             push_val = $('#stack_value').val();
             if (push_val === '') {
                 return alert('empty field, please insert value');
             }
-            item = $('<li id=li>' + push_val + '</li>');
-            stack.push(item);
+            item = $('<p id="li">' + push_val + '</p>');
+            c.push(item);
             $('#stack_ul').prepend(item);
             clearTxt();
         });
-        
-        
+
+
         $('#pop_btn').click(function () {
-            var pop;
-            if (!stack.isEmpty()) {
-                pop = stack.pop();
+            var pop,
+                c = $('.stack-active').data('collection').value;
+            if (!c.isEmpty()) {
+                pop = c.pop();
                 pop.remove();
             } else {
                 alert('the stack is empty!!');
             }
             clearTxt();
-        
+
         });
-     
-        
+
+
         $('#peek_btn').click(function () {
-            var peek;
-            if (!stack.isEmpty()) {
-                peek = stack.peek();
+            var peek,
+                c = $('.stack-active').data('collection').value;
+            if (!c.isEmpty()) {
+                peek = c.peek();
                 console.log(peek);
                 alert('current value in top is: ' + peek.text());
             } else {
@@ -141,40 +119,49 @@ $(document).ready(function () {
             }
             $('#stack_value').val('');
         });
-        
+
         $('#enqueue_btn').click(function () {
-            var queue_val, item;
+            var queue_val,
+                item,
+                c = $('.queue-active').data('collection').value;
             queue_val = $('#queue_value').val();
-            if (queue_val === '') { return alert('empty field, please insert value');}
-            item = $('<li id=li>' + queue_val + '</li>');
-            queue.enqueue(item);
+            if (queue_val === '') { 
+                return alert('empty field, please insert value');
+            }
+            item = $('<p id="li">' + queue_val + '</p>');
+            c.enqueue(item);
             $('#queue_ul').append(item);
-            $("#queue_value").val('');
+            clearTxt();
         });
-        
+
         $('#dequeue_btn').click(function () {
-            var dequeue;
-            if (!queue.isEmpty()) {
-                dequeue = queue.dequeue();
+            var dequeue,
+                c = $('.queue-active').data('collection').value;
+            if (!c.isEmpty()) {
+                dequeue = c.dequeue();
                 dequeue.remove();
             } else {
                 alert('the stack is empty!!');
             }
-            $('#queue_value').val('');
-            
+            clearTxt();
+
         });
-        
+
         $('#peek_btn2').click(function () {
-            var peek;
-            if (!queue.isEmpty()) {
-                peek = stack.peek();
+            var peek,
+                c = $('.queue-active').data('collection').value;
+            if (!c.isEmpty()) {
+                peek = c.peek();
                 console.log(peek);
                 alert('current value in top is: ' + peek.text());
             } else {
                 alert('the stack is empty!!');
             }
-            $('#queue_value').val('');
+            clearTxt();
         });
-    }());
-});
-  
+
+    }
+    
+    c.UI = UI;
+    return c;
+}(jQuery, collections || {}));
---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    'use strict';
+    collections.Stack();
+    collections.Queue();
+    collections.UI();
+
+
+
+});
